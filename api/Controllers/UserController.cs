8e26972c0219dccff266e822aef0978576befefa
dljimenez2324 <dljimenez2324@gmail.com>
2024-07-30 20:08:09 -0700

    database context, started controllers with our add user and service

--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.DTO;
+using api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -10,6 +12,24 @@ namespace api.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly UserService _data;
+        public UserController(UserService dataFromService)
+        {
+            _data = dataFromService;
+        }
         
+        //Add a user
+        [HttpPost]
+        public bool AddUser (CreateAccountDTO UserToAdd)
+        {
+            return _data.AddUser(UserToAdd);
+        }  
+
+
+        //Update user Account
+        //Delete user Account
+    
+
+
     }
 }
--- api/Services/UserService.cs
+++ api/Services/UserService.cs
@@ -2,11 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.DTO;
+using api.Services.Context;
 
 namespace api.Services
 {
     public class UserService
     {
+        private readonly DataContext _context;
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // These are our helper functions to  check if our user exists so this will hold some logic
         
+        // Does User Exist
+        public bool DoesUserExist(string username)
+        {
+            // check our tables to see if the user name is contained within it
+            // check if one item matches our condition .. that item will be returned
+            // if no items matches it will return null
+            // if multiple items match it will return an error
+        }
+
+        // Adding user function
+        public bool AddUser(CreateAccountDTO userToAdd)
+        {
+            
+        }
     }
 }
