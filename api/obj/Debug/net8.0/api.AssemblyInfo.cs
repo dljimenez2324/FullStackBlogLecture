8602adbc1d531f73a94697580d252bdd8c6fba31
dljimenez2324 <dljimenez2324@gmail.com>
2024-12-11 21:27:40 -0800

    random files like cache updated

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb6e31cadc328e4ffc9d1d41b4996a021fb13503")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c1ccb90b69c80705416deecc33d87fc3eebe46")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
